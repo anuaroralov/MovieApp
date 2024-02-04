c4b801ded8fc34fe98a296c65c120fbbea150e8a
Anuar Oralov <anuar.oralov@gmail.com>
2024-02-04 18:15:26 +0600

    init

---
+++ app/src/main/java/com/anuar/movieapp/Presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anuar.movieapp.Presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anuar.movieapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
