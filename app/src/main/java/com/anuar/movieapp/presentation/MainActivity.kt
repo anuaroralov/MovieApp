33f183a81d4e423ce37eaf5682088019ce134a54
Anuar Oralov <anuar.oralov@gmail.com>
2024-02-24 19:51:49 +0600

    adding bottom nav

--- app/src/main/java/com/anuar/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/anuar/movieapp/presentation/MainActivity.kt
@@ -3,15 +3,21 @@ package com.anuar.movieapp.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.anuar.movieapp.R
+import com.anuar.movieapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel by lazy {
-        ViewModelProvider(this)[MyViewModel::class.java]
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigation)
+        navView.setupWithNavController(navController)
 
     }
 }
